3fd34ab490d990d8e82a34b9ceb64a0f42f26c58
Md. Tanvir Ahmed Siddique <tanvir.24633@gmail.com>
2026-09-09 12:59:24 +0600

    built APIs: /get-property and /images

--- server.js
+++ server.js
@@ -1,40 +1,45 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 
 const app = express();
 
 const PORT = 3000;
 
+app.get("/get-property", (req, res) => {
+    let file = "most_popular.json";
 
-/* Serve project files */
+    if (req.query["highest-price"] === "true") file = "highest_price.json";
+    if (req.query["lowest-price"] === "true") file = "lowest_price.json";
 
-app.use(
-    express.static(__dirname)
-);
+    const data = JSON.parse(fs.readFileSync(path.join(__dirname, "data", file)));
+    const items = data.Result.Items;
+    const result = req.query.limit ? items.slice(0, Number(req.query.limit)) : items;
 
+    res.json(result);
+});
 
-/* Serve installed npm packages */
+app.get("/images", (req, res) => {
+    const data = JSON.parse(fs.readFileSync(path.join(__dirname, "data", "image-links.json")));
+    let img_count = req.query.count ? Number(req.query.count) : 10;
+    img_count = Math.min(img_count, data.images.length);
 
-app.use(
-    "/node_modules",
-    express.static(
-        path.join(
-            __dirname,
-            "node_modules"
-        )
-    )
-);
+    res.json(data.images.slice(-img_count));
+});
 
+// TEST API -------- O B S O L E T E -------------
+app.get("/dev", (req, res) => {
+    res.json({data: {
+        "name": "MD. TANVIR AHMED SIDDIQUE",
+        "Designation": "SWE INTERN",
+        "Organization": "W3 Engineers Ltd."}});
+});
+// -----------------------------------------------
 
-/* Start server */
+app.use(express.static(__dirname));
 
-app.listen(
-    PORT,
-    function () {
+app.use("/node_modules", express.static(path.join(__dirname, "node_modules")));
 
-        console.log(
-            `Server running at http://localhost:${PORT}`
-        );
-
-    }
-);
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
